61b2c311438a791e0c021fa69631c01c797d45e9
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-01-19 11:59:40 +0530

    Alert is working

--- otp-login/src/App.jsx
+++ otp-login/src/App.jsx
@@ -1,13 +1,47 @@
+import React, { useState } from 'react';
 import './App.css'
-import PhoneNumber from './components/PhoneNumber'
 
 function App() {
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [displayOTP, setDisplayOTP] = useState(false)
+
+  const handleInputChange = (event) => {
+    setPhoneNumber(event.target.value);
+    console.log(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    // handle phone number validation
+    if (phoneNumber.length >= 10) {
+      setDisplayOTP(true);
+    }
+    else {
+      alert("Enter valid phone number.");
+      return;
+    }
+
+    alert("Form submission successful.")
+  }
+
   return (
     <div className='heading'>
       Log in or sign up to continue
-      <PhoneNumber />
+      { }
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Enter phone number'
+          value={phoneNumber}
+          onChange={handleInputChange}
+        />
+        <button type='submit'>
+          Submit
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ otp-login/src/components/EnterOTP.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EnterOTP() {
+  return (
+    <div>EnterOTP</div>
+  )
+}
+
+export default EnterOTP
