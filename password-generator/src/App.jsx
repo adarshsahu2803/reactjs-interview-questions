7a14d4e058b4b9f930ec01577831b052bbc09fca
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-01-20 15:40:50 +0530

    Added basic UI

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,15 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [password, setPassword] = useState('')
+  const [passLength, setPassLength] = useState(6)
+  const [hasNumber, setHasNumber] = useState(false)
+  const [hasChar, setHasChar] = useState(false)
 
   return (
-    <>
-      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
-    </>
+    <div className='passGen bg-blue text-white'>
+      <h1 className='passGen text-4xl text-cent'>Password Generator</h1>
+      <div>
+        <input
+          type='text'
+          value={"a;dkfjadhjhf3sdf443"}
+          readOnly
+        />
+        <button>copy</button>
+      </div>
+      <div className='range-check'>
+        <label>
+          <input
+            type='range'
+            min={6}
+            max={100}
+            defaultValue={10}
+          />
+          Length: 10
+        </label>
+        <label>
+          <input
+            type='checkbox'
+          />
+          Numbers
+        </label>
+        <label>
+          <input
+            type='checkbox'
+          />
+          Characters
+        </label>
+      </div>
+    </div>
   )
 }
 
