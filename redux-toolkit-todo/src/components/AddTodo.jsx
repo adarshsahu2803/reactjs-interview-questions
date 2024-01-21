c6f8535d7cfb29db67038fad264735402255ae0c
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-01-22 00:38:10 +0530

    Added addTodo/UI

---
+++ redux-toolkit-todo/src/components/AddTodo.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { addTodo } from '../features/todo/todoSlices'
+
+function AddTodo() {
+    const [input, setInput] = useState('')
+    const dispatch = useDispatch()
+
+    const addTodoHandler = (e) => {
+        e.preventDefault()
+        dispatch(addTodo(input))
+        setInput('')
+    }
+
+    return (
+        <form onSubmit={addTodoHandler} className='space-x-3 mt-12'>
+            <input 
+                type='text'
+                className='bg-gray-800 rounded border border-gray-700
+                 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900
+                 text-base outline-none text-gray-100 py-1 px-3 leading-8
+                 transition-colors duration-200 ease-in-out'
+                 placeholder='Enter a Todo...'
+                 value={input}
+            />
+        </form>
+        )
+}
+
+export default AddTodo
