395859db529cf54417c03474b540d7ed7ec9048a
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-01-21 22:58:02 +0530

    Initial commit

---
+++ redux-toolkit-todo/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>Redux Toolkit Todo App</>
+    )
+}
+
+export default App
