06080b9171fbb5826892fbea6924b862ae54a5a7
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-02-22 11:05:35 +0530

    Added tailwind css

--- context-api-shopping-cart/src/App.jsx
+++ context-api-shopping-cart/src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
+import Cart from './components/Cart'
+import Product from './components/Product'
+import Home from './components/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/product' element={<Product />} />
+      <Route path='/cart' element={<Cart />} />
+    </Routes>
   )
 }
 
