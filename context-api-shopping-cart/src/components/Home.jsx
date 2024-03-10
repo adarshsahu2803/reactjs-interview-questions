f7470c7d6bb3d13127fdccfb484cbb9d0bea870f
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-03-10 23:33:57 +0530

    Failed to load faker resources

--- context-api-shopping-cart/src/components/Home.jsx
+++ context-api-shopping-cart/src/components/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
+import faker from 'faker'
 
 function Home() {
+  const productArray = [...Array(20)].map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.random.image(),    
+  }))
+
+  const [product] = useState(productArray)
+
   return (
-    <div className="text-3xl text-center">
-      React Context API
+    <div>
+      <div className="text-3xl text-center">React Context API</div>
       <Navbar />
+      {product.map(prod => (
+        <span>{prod.name}</span>
+      ))}
     </div>
   )
 }
