6e2630a6c75329306df32c03f1ce5c3e2a665795
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-04-05 19:36:37 +0530

    Added product array

--- context-api-shopping-cart/src/components/Home.jsx
+++ context-api-shopping-cart/src/components/Home.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from 'react'
 import Navbar from './Navbar'
 import { faker } from '@faker-js/faker'
+import {v4 as uuidv4} from 'uuid';
 
 function Home() {
   const productArray = [...Array(20)].map(() => ({
-    // id: faker.datatype.,
+    id: uuidv4(),
     name: faker.commerce.productName(),
     price: faker.commerce.price(),
-    // image: faker.image.a,    
+    image: faker.image.url(), 
   })) 
-
-  // const [product] = useState(productArray)
 
   return (
     <div>
       <div className="text-3xl text-center">React Context API</div>
       <Navbar />
-      {productArray.map(prod => (
-        <span>{prod.name}</span>
+      {productArray.map(product => (
+        <div key={product.id}>
+          <span>{product.name}</span>
+          <span>{product.price}</span>
+          <img src={product.image} alt={product.name} />
+        </div>
       ))}
     </div>
   )
